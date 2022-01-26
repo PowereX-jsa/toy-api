50792d46fff9e9f204738be8c5d6b0eaed2eca19
Peter Cvacho <peter.cvacho@powerex.io>
2022-01-26 12:58:00 +0000

    Initial commit for toy API

---
+++ R/read_quandl_unemployement_data.R
@@ -0,0 +1,23 @@
+#' Read Unemployement US data
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \donotrun{read_quandl_unemployement_data()}
+read_quandl_unemployement_data <- function(){
+  logging::loginfo("Start reading Quandl data.")
+
+  tryCatchLog::tryCatchLog({
+    res <- Quandl::Quandl("FRED/NROUST") %>% data.table::as.data.table()
+
+    res[, Date := lubridate::ymd(Date)]
+  },
+  error = function(e){
+    stop("Error in reading Quandl data, I must stop")
+  })
+
+  logging::loginfo("Finished reading Quandl data.")
+
+  res
+}
